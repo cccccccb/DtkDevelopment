c5cf81243c803c14db664565b6855de7dbcb58a5
Chen Bin <chenbin@uniontech.com>
2022-09-01 18:01:10 +0800

    update dockerfile

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM        x11docker/deepin
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT  "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ls
+mv /etc/apt/sources.list /etc/apt/sources.list.back
+mv .environment/sources.list /etc/apt/
+cat /etc/apt/sources.list
+apt update
+apt install -y build-essential
+apt build-dep -y libdtkcore-dev
+apt build-dep -y libdtkgui-dev
+apt build-dep -y libdtkwidget-dev
+
+cd dtkcommon
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+cmake --build . -j12
+cmake --install .
+cd ../../dtkcore
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+cmake --build . -j12
+cmake --install .
+cd ../../dtkgui
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+cmake --build . -j12
+cmake --install .
+cd ../../dtkwidget
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+cmake --build . -j12
+cmake --install .
