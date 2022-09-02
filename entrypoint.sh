ace48631d1b01fcdf5a1e0df8548ea6bf9bedb51
Chen Bin <chenbin@uniontech.com>
2022-09-02 12:17:00 +0800

    update cmake location

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,7 @@ mv /etc/apt/sources.list /etc/apt/sources.list.back
 mv .environment/sources.list /etc/apt/
 cat /etc/apt/sources.list
 apt update
-apt install -y build-essential
-apt install -y git
-apt install -y libssh-dev
-apt install -y libgtest-dev
-apt install -y libgmock-dev
-apt install -y librsvg2-dev
-apt install -y libxext-dev
-apt install -y libudev-dev
-apt install -y x11proto-xext-dev
-apt install -y libxcb-util0-dev
-apt install -y libstartup-notification0-dev
-apt install -y libmtdev-dev
-apt install -y libegl1-mesa-dev
-apt install -y libudev-dev
-apt install -y libfontconfig1-dev
-apt install -y libfreetype6-dev
-apt install -y libglib2.0-dev
-apt install -y libxrender-dev
-apt install -y libxi-dev
-apt install -y libcups2-dev
+apt install -y build-essential git libssh-dev libgtest-dev libgmock-dev librsvg2-dev libxext-dev libudev-dev x11proto-xext-dev libxcb-util0-dev libstartup-notification0-dev libmtdev-dev libegl1-mesa-dev libudev-dev libfontconfig1-dev libfreetype6-dev libglib2.0-dev libxrender-dev libxi-dev libcups2-dev
 
 git submodule init
 git submodule update
@@ -38,16 +19,15 @@ make -j16
 make install
 cd ../../../
 echo "cmake build finished"
-which cmake
 
 # build dtkcommon 
 echo "Build dtkcommon"
 cd dtkcommon
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
-cmake --build . -j12
-cmake --install .
+../../QtInstalled/5.15.3/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+../../QtInstalled/5.15.3/bin/cmake --build . -j12
+../../QtInstalled/5.15.3/bin/cmake --install .
 echo "Finished dtkcommon"
 
 # build dtkcore
@@ -55,9 +35,9 @@ echo "Build dtkcore"
 cd ../../dtkcore
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
-cmake --build . -j12
-cmake --install .
+../../QtInstalled/5.15.3/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+../../QtInstalled/5.15.3/bin/cmake --build . -j12
+../../QtInstalled/5.15.3/bin/cmake --install .
 echo "Finished dtkcore"
 
 # build dtkgui
@@ -65,9 +45,9 @@ echo "Build dtkgui"
 cd ../../dtkgui
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
-cmake --build . -j12
-cmake --install .
+../../QtInstalled/5.15.3/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+../../QtInstalled/5.15.3/bin/cmake --build . -j12
+../../QtInstalled/5.15.3/bin/cmake --install .
 echo "Finished dtkgui"
 
 # build dtkwidget
@@ -75,7 +55,7 @@ echo "Build dtkwidget"
 cd ../../dtkwidget
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
-cmake --build . -j12
-cmake --install .
+../../QtInstalled/5.15.3/bin/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../../QtInstalled/5.15.3/" -DCMAKE_INSTALL_PREFIX="../../QtInstalled/5.15.3/" -DBUILD_DOCS=false ..
+../../QtInstalled/5.15.3/bin/cmake --build . -j12
+../../QtInstalled/5.15.3/bin/cmake --install .
 echo "Finished dtkwidget"
