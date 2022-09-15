79fb5e1dd862fa3eebb25c6a59b95ab680c623aa
Chen Bin <chenbin@uniontech.com>
2022-09-15 17:35:16 +0800

    modify the path error

--- DtkEnvironment/packages/com.deepin.dtk/data/ReplaceUserPath.sh
+++ DtkEnvironment/packages/com.deepin.dtk/data/ReplaceUserPath.sh
@@ -4,9 +4,16 @@ TargetPath=$1
 echo $TargetPath
 
 DtkDirName=`ls $TargetPath | grep DtkDevelopment`
-TargetDtkPath=${TargetPath}/${DtkDirName}
-echo ${TargetDtkPath}
+
+if [ ! -n "$DtkDirName" ]; then
+    TargetDtkPath=${TargetPath}
+else
+    TargetDtkPath=${TargetPath}/${DtkDirName}
+fi
 
 find $TargetPath -name "qt_lib_dtk*.pri" | xargs sed -i "s|\/github\/workspace\/QtInstalled\/5.15.3|${TargetDtkPath}|"
+find $TargetPath -name "Dtk*Config.cmake" | xargs sed -i "s|\/github\/workspace\/QtInstalled\/5.15.3|${TargetDtkPath}|"
 find $TargetPath -name "*.pc" | xargs sed -i "s|\[_PKG_CONFIG_PATH_MODULE_\]|${TargetDtkPath}|"
+find $TargetPath -name "*.pc" | xargs sed -i "s|\/github\/workspace\/QtInstalled\/5.15.3|${TargetDtkPath}|"
+
 echo "Replace Done"
